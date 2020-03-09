cefe840894be36e3fba6b9f6fc9e9c3e1a0b07cb
Raymond Cao <raymond17064@gmail.com>
2020-03-09 01:34:44 -0700

    Frame limiter removed
    Actually removed fream limiter

--- detect_faces_video.py
+++ detect_faces_video.py
@@ -34,7 +34,7 @@ net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 # initialize the video stream and allow the cammera sensor to warmup
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
-time.sleep(2.0)
+# time.sleep(2.0)
 
 # loop over the frames from the video stream
 while True:
